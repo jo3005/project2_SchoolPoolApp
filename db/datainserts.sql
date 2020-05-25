48d1ab4cfc375efd2991e761afbf1d8b9964a2d9
jo3005 <jo@caswa.com>
2020-05-25 22:35:48 +0800

    Designed database schema and setup tables.

---
+++ db/datainserts.sql
@@ -0,0 +1,24 @@
+insert into childseattype (description) values ('none');
+insert into childseattype (description) values ('childseat');
+insert into childseattype (description) values ('booster');
+
+insert into relationships (relationship) values ('Mother');
+insert into relationships (relationship) values ('Father');
+insert into relationships (relationship) values ('Step-Mother');
+insert into relationships (relationship) values ('Step-Father');
+insert into relationships (relationship) values ('Grandmother - maternal');
+insert into relationships (relationship) values ('Grandmother - paternal');
+insert into relationships (relationship) values ('Grandfather - maternal');
+insert into relationships (relationship) values ('Grandfather - paternal');
+insert into relationships (relationship) values ('Older Sibling');
+insert into relationships (relationship) values ('Nanny');
+insert into relationships (relationship) values ('Other');
+
+insert into australiastates (stateAbbr,stateName) values ('WA','Western Australia');
+insert into australiastates (stateAbbr,stateName) values ('SA','South Australia');
+insert into australiastates (stateAbbr,stateName) values ('NT','Northern Territory');
+insert into australiastates (stateAbbr,stateName) values ('QLD','Queensland');
+insert into australiastates (stateAbbr,stateName) values ('NSW','New South Wales');
+insert into australiastates (stateAbbr,stateName) values ('VIC','Victoria');
+insert into australiastates (stateAbbr,stateName) values ('TAS','Tasmania');
+insert into australiastates (stateAbbr,stateName) values ('ACT','Australian Capital Territory');
