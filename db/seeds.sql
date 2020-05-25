c41b5c7e4b1362e3c18bc333e41a11b263b9fba7
Mwansa <Mwansad.mwango@gmail.com>
2020-05-25 21:03:12 +0800

    Initial boiler plate folder based on blogger CRUD

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Juicy Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggy Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
