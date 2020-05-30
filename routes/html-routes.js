62da39c727d2695a1778623ba39de0b33a6f12c2
Mwansa <Mwansad.mwango@gmail.com>
2020-05-30 19:07:37 +0800

    Added passport authentication boiler plate to blogger CRUD

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,18 +5,46 @@
 // Dependencies
 // =============================================================
 var path = require("path");
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-// Routes
+// ROUTES
 // =============================================================
 module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
+  // PASSPORT AUTHENTICATION STATIC ROUTES
+  // =============================================================
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+
+  // // index route loads view.html
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/blog.html"));
+  // });
+
+  // TODO: REQUESTOR STATIC ROUTES
+  // =============================================================
   app.get("/cms", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/cms.html"));
   });
@@ -25,5 +53,8 @@ module.exports = function(app) {
   app.get("/blog", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/blog.html"));
   });
+
+  // TODO: DRIVER STATIC ROUTES
+  // =============================================================
 
 };
--- server.js
+++ server.js
@@ -5,12 +5,14 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
 var PORT = process.env.PORT || 8080;
-
+var app = express();
 // Requiring our models for syncing
 var db = require("./models");
 
@@ -21,6 +23,11 @@ app.use(express.json());
 // Static directory
 app.use(express.static("public"));
 
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
 // =============================================================
 require("./routes/api-routes.js")(app);
@@ -28,8 +35,9 @@ require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function() {
+db.sequelize.sync().then(function() { // creates tables for all models that were defined (i.e. using the define method on an instance of Sequelize)
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
+ 
