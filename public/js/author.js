47e5f0f262243c529da0e9d7b212e3d8512b5109
Mwansa <Mwansad.mwango@gmail.com>
2020-06-07 12:20:05 +0800

    Initial commit of driver details page (not working)

--- public/js/author.js
+++ public/js/author.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   var authorContainer = $(".author-container");
   // Adding event listeners to the form to create a new object, and the button to delete
   // an Author
-  $(document).on("submit", "#author-form", handleAuthorFormSubmit);
+  // $(document).on("submit", "#author-form", handleAuthorFormSubmit);
   $(document).on("click", ".delete-author", handleDeleteButtonPress);
 
   // Getting the initial list of Authors
