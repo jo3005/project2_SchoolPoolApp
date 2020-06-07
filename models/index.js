72ee254b015b9cedc20bb4132847e594e313049d
jo3005 <jo@caswa.com>
2020-06-08 03:43:45 +0800

    Changed config.js to use env variables for localhost. Now works on heroku and localhost

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config")[env];
 var db = {};
-console.log(config);
+//console.log(config);
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(config.use_env_variable, config);
